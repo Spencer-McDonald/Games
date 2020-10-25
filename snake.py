b2debab4fe5cc8077c4c4e2813e136081f7ceefb
Spencer McDonald <spencer@mcdonald.dev>
2020-10-24 22:00:37 -0500

    Adjusting credits to tutorial.

--- snake.py
+++ snake.py
@@ -7,7 +7,7 @@ black = (0, 0, 0)
 red = (255, 0, 0)
  
 dis = pygame.display.set_mode((800, 600))
-pygame.display.set_caption('Snake Game by Edureka')
+pygame.display.set_caption('Snake Game Tutorial written by Edureka')
  
 game_over = False
  
